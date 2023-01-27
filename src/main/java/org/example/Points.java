975b3656819aa7dd3c79df7de750665b536e51f8
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-01-27 14:57:04 +0530

    inital comment

--- src/main/java/org/example/Points.java
+++ src/main/java/org/example/Points.java
@@ -1,7 +1,40 @@
 package org.example;
+import java.util.*;
+import java.util.logging.*;
 
+class Coordi implements Cloneable{
+    String x1;
+    String y1;
+    Coordi(String x1,String y1){
+        this.x1=x1;
+        this.y1=y1;
+    }
+    public boolean equal(String x2, String y2){
+        return x1.equals(x2) && y1.equals(y2);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
 public class Points {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)throws CloneNotSupportedException {
+        Scanner sc=new Scanner(System.in);
+        Logger l = Logger.getLogger("com.api.jar");
+        l.info("Welcome guys");
+        l.info("Enter the co-ordinates X: ");
+        String x1=sc.nextLine();
+        l.info("Enter the co-ordinates Y: ");
+        String y1=sc.nextLine();
+        Coordi c1=new Coordi(x1,y1);
+        l.info("Enter the co-ordinate X to check:");
+        String x2=sc.nextLine();
+        l.info("Enter the co-ordinates Y to check: ");
+        String y2=sc.nextLine();
+        Coordi c2=(Coordi)c1.clone();
+        l.info("The result is: "+c2.equal(x2,y2));
+
+
     }
 }
