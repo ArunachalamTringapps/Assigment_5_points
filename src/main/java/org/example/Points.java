32ae2ebb091dcb37c372d0a46fd64cc023a4702b
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-01-30 15:11:20 +0530

    inital commit

--- src/main/java/org/example/Points.java
+++ src/main/java/org/example/Points.java
@@ -3,9 +3,9 @@ import java.util.*;
 import java.util.logging.*;
 
 class Coordi implements Cloneable {
-    String x1;
-    String y1;
-    Coordi(String x1,String y1){
+    int x1;
+    int y1;
+    Coordi(int x1,int y1){
         this.x1=x1;
         this.y1=y1;
     }
@@ -15,25 +15,29 @@ class Coordi implements Cloneable {
         return super.clone();
     }
 
-    public boolean equalling(String x2, String y2){
-        return x1.equals(x2) && y1.equals(y2);
+    public boolean equalling(int x2, int y2){
+        String a1=String.valueOf(x1);
+        String b1=String.valueOf(y1);
+        String a2=String.valueOf(x2);
+        String b2=String.valueOf(y2);
+        return a1.equals(a2) && b1.equals(b2);
     }
 
 }
 public class Points {
-    public static void main(String[] args)throws CloneNotSupportedException  {
+    public static void main(String[] args)throws Exception  {
         Scanner sc=new Scanner(System.in);
         Logger l = Logger.getLogger("com.api.jar");
         l.info("Welcome guys");
         l.info("Enter the co-ordinates X: ");
-        String x1=sc.nextLine();
+        int x1=sc.nextInt();
         l.info("Enter the co-ordinates Y: ");
-        String y1=sc.nextLine();
+        int y1=sc.nextInt();
         Coordi c1=new Coordi(x1,y1);
         l.info("Enter the co-ordinate X to check:");
-        String x2=sc.nextLine();
+        int x2=sc.nextInt();
         l.info("Enter the co-ordinates Y to check: ");
-        String y2=sc.nextLine();
+        int y2=sc.nextInt();
         Coordi c2=(Coordi) c1.clone();
         l.log(Level.INFO, () -> "The result is: " + c2.equalling(x2,y2));
     }
