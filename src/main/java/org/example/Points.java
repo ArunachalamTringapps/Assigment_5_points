2ec27a0ef84a05356164cf4258a0818a2d369420
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-02-28 06:10:56 +0530

    inital commit

--- src/main/java/org/example/Points.java
+++ src/main/java/org/example/Points.java
@@ -2,37 +2,11 @@ package org.example;
 import java.util.*;
 import java.util.logging.*;
 
-class Coordi {
-    int x1;
-    int y1;
 
-    Coordi(int x1,int y1){
-        this.x1=x1;
-        this.y1=y1;
-    }
-
-    public Coordi cloneexample()  {
-        try {
-            return (Coordi) super.clone();
-        }catch (Exception ex){
-            return this;
-        }
-
-    }
-
-    public boolean equalling(int x2, int y2){
-        String a1=String.valueOf(x1);
-        String b1=String.valueOf(y1);
-        String a2=String.valueOf(x2);
-        String b2=String.valueOf(y2);
-        return a1.equals(a2) && b1.equals(b2);
-    }
-
-}
 public class Points {
     public static void main(String[] args)  {
         Scanner sc=new Scanner(System.in);
-        Logger l = Logger.getLogger("com.api.jar");
+        Logger l = Logger.getLogger(Points.class.getName());
         l.info("Welcome guys");
         l.info("Enter the co-ordinates X: ");
         try {
